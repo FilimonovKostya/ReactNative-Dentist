93b17966c7ce95a3d81c300ac7f81463d20a081e
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-06-09 14:42:13 +0400

    Bottom navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import Main from "./Main";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {StatusBar} from "expo-status-bar";
 import {Button, Text, View} from "react-native";
 import {RootStackParamList, UsersScreeProps} from "./navigationTypes";
 
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+// const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
 
 const Users = ({navigation}: UsersScreeProps) => {
     return (
@@ -21,14 +24,26 @@ const Users = ({navigation}: UsersScreeProps) => {
     )
 }
 
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
 export default function App() {
     return (
         <NavigationContainer>
             <StatusBar style="auto"/>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Main}/>
-                <Stack.Screen name="Users" component={Users}/>
-            </Stack.Navigator>
+            {/*<Stack.Navigator>*/}
+            {/*    <Stack.Screen name="Home" component={Main}/>*/}
+            {/*    <Stack.Screen name="Users" component={Users}/>*/}
+            {/*</Stack.Navigator>*/}
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={Main} />
+                <Tab.Screen name="Users" component={Users} />
+            </Tab.Navigator>
         </NavigationContainer>
     );
 }
--- Main.tsx
+++ Main.tsx
@@ -2,12 +2,10 @@ import {Button, Text, View} from "react-native";
 import {HomeScreeProps} from "./navigationTypes";
 
 
-const Main = ({route, navigation}: HomeScreeProps) => {
-    console.log('route',route.params)
+const Main = ({route, navigation}: any) => {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>HOme</Text>
-            <Text>{route.params.name}</Text>
             <Button title={'Jump to Users Screen'} onPress={() => navigation.navigate('Users')}/>
         </View>
     )

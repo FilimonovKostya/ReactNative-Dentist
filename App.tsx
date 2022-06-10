b4a120f2e3e167e6e3bb76ff295b5e9e667fa196
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-06-10 20:25:28 +0400

    added prettier

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import {Text, View} from "react-native";
+import { Text, View } from 'react-native'
 
 export default function App() {
-    return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text style={{color: 'black', marginTop: 20}}>Hi</Text>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: 'black', marginTop: 20 }}>Hi</Text>
+    </View>
+  )
 }
-
